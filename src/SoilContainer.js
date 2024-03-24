85033bebaa785eaced2b40fb1a9b27d63ab587da
ineedmoney527 <junheng2003@hotmail.com>
2024-03-24 13:43:03 +0800

    ss

--- src/SoilContainer.js
+++ src/SoilContainer.js
@@ -117,206 +117,130 @@ const SoilContainer = () => {
       },
     ],
   };
-  const heatChartData = {
-    options: {
-      chart: {
-        type: "heatmap",
-      },
-      dataLabels: {
-        enabled: true,
-      },
-      colors: ["red", "yellow", "green"], // Heatmap color
-      xaxis: {
-        categories: [
-          ["Zone A"],
-          ["Zone B"],
-          ["Zone C"],
-
-          // Add more groups of three zones as needed
-        ],
-      },
-      plotOptions: {
-        heatmap: {
-          shadeIntensity: 0.5,
-          radius: 0,
-          useFillColorAsStroke: true,
-          colorScale: {
-            ranges: [
-              {
-                from: -30,
-                to: 5,
-                name: "extreme",
-                color: "#00A100",
-              },
-              {
-                from: 6,
-                to: 20,
-                name: "high",
-                color: "#128FD9",
-              },
-              {
-                from: 21,
-                to: 45,
-                name: "medium",
-                color: "#FFB200",
-              },
-              {
-                from: 46,
-                to: 55,
-                name: "low",
-                color: "#FF0000",
-              },
-            ],
-          },
-        },
-      },
-      title: {
-        text: "Soil Fertility Heat Map",
-        align: "center",
-      },
-    },
-    series: [
-      {
-        name: "Row 1",
-        data: [
-          {
-            x: "W1",
-            y: 22,
-          },
-          {
-            x: "W2",
-            y: 2,
-          },
-          {
-            x: "W3",
-            y: 50,
-          },
-          {
-            x: "W4",
-            y: 32,
-          },
-        ],
-      },
-      {
-        name: "Series 2",
-        data: [
-          {
-            x: "W1",
-            y: 3,
-          },
-          {
-            x: "W2",
-            y: 10,
-          },
-          {
-            x: "W3",
-            y: 12,
-          },
-          {
-            x: "W4",
-            y: 43,
-          },
-        ],
-      },
-    ],
-  };
 
   return (
-    <div>
+    <div style={{ padding: "50px" }}>
       <h1>Soil Management Dashboard</h1>
 
       {soilData && (
         <div style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
-          <h2>pH Level</h2>
-          <div
-            style={{
-              flex: 1,
-              width: "400px",
-              height: "200px",
-            }}
-          >
-            <GaugeComponent
-              minValue={0}
-              maxValue={14}
-              height={200}
-              width={200}
-              value={2}
-              type="radial"
-              labels={{
-                tickLabels: {
-                  type: "inner",
-                },
+          {/* pH Level and Nutrient Value */}
+          <div style={{ display: "flex", gap: "20px" }}>
+            <div
+              className="gauge-container"
+              style={{
+                borderRadius: "15px",
+                border: "2px solid #000",
+                padding: "50px",
+                flex: "1",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                gap: "10px",
+                border: "1px solid #000",
+                boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)", // Added shadow
               }}
-              arc={{
-                subArcs: [
-                  { limit: 2, color: "red", showTick: true }, // pH between 0 and 3 (Red)
-                  { limit: 4, color: "orange", showTick: true }, // pH between 3 and 5 (Orange)
-                  { limit: 6, color: "yellow", showTick: true }, // pH betcween 5 and 7 (Yellow)
-                  { limit: 8, color: "green", showTick: true }, // pH between 7 and 9 (Green)
-                  { limit: 10, color: "#0496C7", showTick: true }, // pH between 9 and 11 (Blue)
-                  { limit: 12, color: "BLUE", showTick: true }, // pH between 11 and 14 (Violet)
-                  { limit: 14, color: "purple", showTick: true },
-                ],
+            >
+              <div>
+                <h2 style={{ marginBottom: "100px" }}>pH value</h2>
+              </div>
+              <GaugeComponent
+                style={{
+                  marginBottom: "100px",
+                }}
+                minValue={0}
+                maxValue={14}
+                height={200}
+                width={200}
+                value={3}
+                type="radial"
+                labels={{
+                  tickLabels: {
+                    type: "inner",
+                  },
+                }}
+                arc={{
+                  subArcs: [
+                    { limit: 2, color: "red", showTick: true }, // pH between 0 and 3 (Red)
+                    { limit: 4, color: "orange", showTick: true }, // pH between 3 and 5 (Orange)
+                    { limit: 6, color: "yellow", showTick: true }, // pH between 5 and 7 (Yellow)
+                    { limit: 8, color: "green", showTick: true }, // pH between 7 and 9 (Green)
+                    { limit: 10, color: "#0496C7", showTick: true }, // pH between 9 and 11 (Blue)
+                    { limit: 12, color: "BLUE", showTick: true }, // pH between 11 and 14 (Violet)
+                    { limit: 14, color: "purple", showTick: true },
+                  ],
+                }}
+                pointer={{
+                  elastic: true,
+                  animationDelay: 0.3,
+                }}
+              />
+              <div
+                class="alert-container"
+                style={{
+                  width: "80%",
+                  marginLeft: "45px",
+                  backgroundColor: "#f0f0f0",
+                  border: "3px solid #ccc",
+                  borderRadius: "10px",
+                  backgroundColor: "lightyellow",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <h3 style={{ color: "red", fontWeight: "bolder" }}>
+                  Alert : ph Value too low !
+                </h3>
+              </div>
+            </div>
+            <div
+              style={{
+                borderRadius: "15px",
+                border: "2px solid #000",
+                padding: "50px",
+                flex: "1",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                gap: "10px",
+                border: "1px solid #000",
+                boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)", // Added shadow
               }}
-              pointer={{
-                elastic: true,
-                animationDelay: 0.3,
-              }}
-            />
-            <p style={{ fontSize: "25px" }}>pH: {soilData.pH.toFixed(1)}</p>
+            >
+              <div>
+                <h2>Soil Health Indicators</h2>
+              </div>
+
+              <Radar
+                style={{ flex: "1" }}
+                title="Soil Nutrient Content"
+                data={chartData}
+              />
+            </div>
           </div>
-          {/* Display Soil Moisture and Temperature */}
+
+          {/* Temperature and Moisture Chart */}
           <div
             style={{
-              flex: 1,
-              width: "500px",
-              height: "600px",
+              borderRadius: "15px",
+              border: "2px solid #000",
+              padding: "50px",
+              flex: "1",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              gap: "10px",
+              border: "1px solid #000",
+              boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)", // Added shadow
             }}
           >
             <h2>Soil Moisture and Temperature</h2>
             <p>Moisture: {soilData.soilMoisture}%</p>
             <p>Temperature: {soilData.soilTemperature}°C</p>
-            <TemperatureMoistureChart data={soilData.tempMoisture} />
-          </div>
-          {/* Display Soil Health Indicators */}
-          <div
-            style={{
-              flex: 1,
-              width: "400px",
-              height: "200px",
-            }}
-          >
-            <h2>Soil Health Indicators</h2>
-            <Radar
-              style={{
-                flex: 1,
-                width: "400px",
-                height: "200px",
-              }}
-              title="Soil Nutrient Content"
-              data={chartData}
-            />
-            {/* /* <p>Microbial Activity: {soilData.soilHealth.microbialActivity}%</p>
-            <p>
-              Earthworm Populations: {soilData.soilHealth.earthwormPopulations}{" "}
-              per sqm
-            </p> */}
-          </div>
-          {/* Display Soil Erosion Risk */}
-          {/* <div>
-            <h2>Soil Erosion Risk</h2>
-            <p>{soilData.soilErosionRisk}</p>
-          </div> */}{" "}
-          */
-          {/* Display Soil Fertility Map */}
-          <div>
-            <h2>Soil Fertility Map</h2>
-            <ReactApexChart
-              options={heatChartData?.options}
-              series={heatChartData?.series}
-              type="heatmap"
-              height={350}
-            />
+            <div style={{ width: "100%", height: "100%" }}>
+              <TemperatureMoistureChart data={soilData.tempMoisture} />
+            </div>
           </div>
         </div>
       )}
