a729b84a92bf500f6423ad023f3b217a71c856ba
ineedmoney527 <junheng2003@hotmail.com>
2024-05-01 15:28:34 +0800

    chatbot modification

--- backend/controller/ChatbotController.js
+++ backend/controller/ChatbotController.js
@@ -73,114 +73,6 @@
 // // //       return "";
 // // //     })
 // // //     .join("\n");
-// // // };
-
-// // // //templates and promtps
-// // // const standaloneQuestionTemplate = `Given some conversation history (if any) and a question, convert the question to a standalone question.
-// // // conversation history: {conv_history}
-// // // question: {question}
-// // // (Don't modify if the question is a statement)
-// // // standalone question:`;
-// // // const standaloneQuestionPrompt = PromptTemplate.fromTemplate(
-// // //   standaloneQuestionTemplate
-// // // );
-
-// // // //chaining
-// // // const standaloneQuestionChain = standaloneQuestionPrompt
-// // //   .pipe(llm)
-// // //   .pipe(new StringOutputParser());
-
-// // // const retrieverChain = RunnableSequence.from([
-// // //   (prevResult) => prevResult.standalone_question,
-// // //   retriever,
-// // //   combineDocuments,
-// // // ]);
-
-// // // const answerTemplate = `You have a friendly, helpful and knowledgeable AI assistant who answers questions by combining information from a given context, its own knowledge, and a database.
-
-// // // When the user makes a statement rather than asking a question, I will respond in a friendly and conversational manner, acknowledging the user's input and offering to assist further if needed. I will not assume the user is asking a database-related question unless the context clearly indicates that.
-
-// // // ------------
-// // // QUESTION: {question}
-// // // ------------
-// // // SQL RESPONSE: {text}
-
-// // // NATURAL LANGUAGE RESPONSE:
-
-// // // Let's start by analyzing the SQL response and formulating a natural language response:
-
-// // // Note: A book uniquely identified by book code has a ISBN (referenced to isbn table) which shows all details information such as title, publisher, author, year of the book.
-
-// // // If the question is not directly related to the database such that {text} is "I don't know", I will still do my best to provide a helpful response. I have a broad knowledge base and can engage in general conversation, offer advice, or direct the user to additional resources if needed. My goal is to be a friendly and useful assistant, no matter the nature of the user's input. But don't tell user "I dont have this information in database"
-
-// // // Next, I'll utilize my own knowledge to delve deeper into answering the question if requested by user or if the answer cannot be found in database:
-
-// // // If I'm unable to provide an answer, I'll say "I don't know, sorry."
-
-// // // The final answer is:
-// // // `;
-
-// // // const answerPrompt = PromptTemplate.fromTemplate(answerTemplate);
-
-// // // const answerChain = RunnableSequence.from([
-// // //   {
-// // //     question: (prevResult) => prevResult.question,
-// // //     text: async (prevResult) => {
-// // //       const q = prevResult.question;
-// // //       console.log(prevResult.question);
-// // //       const input = q;
-
-// // //       const result = await executor.invoke({ input });
-
-// // //       console.log(`Got output ${result.output}`);
-// // //       console.log(
-// // //         `Got intermediate steps ${JSON.stringify(
-// // //           result.intermediateSteps,
-// // //           null,
-// // //           2
-// // //         )}`
-// // //       );
-// // //       return result.output;
-// // //     },
-// // //   },
-
-// // //   answerPrompt,
-// // //   llm,
-// // //   new StringOutputParser(),
-// // // ]);
-
-// // // const chain = RunnableSequence.from([
-// // //   {
-// // //     standalone_question: standaloneQuestionChain,
-// // //     original_input: new RunnablePassthrough(),
-// // //   },
-// // //   {
-// // //     context: retrieverChain,
-// // //     // question: ({ original_input }) => original_input.question,
-// // //     question: (prev) => prev.standalone_question,
-
-// // //     conv_history: ({ original_input }) => original_input.conv_history,
-// // //   },
-// // //   answerChain,
-// // // ]);
-
-// // // //request
-// // // router.post("/", async (req, res) => {
-// // //   console.log(process.env.openAIApiKey);
-// // //   const { userQuestion, convHistory } = req.body;
-// // //   const question = userQuestion.trim();
-
-// // //   // Add AI response to history
-// // //   const response = await chain.invoke({
-// // //     question,
-// // //     conv_history: formatConvHistory(convHistory),
-// // //   });
-
-// // //   // Here you would process the question, interact with your AI models, and send back a response
-// // //   res.json({ messages: response });
-// // // });
-
-// // // export default router;
 
 import express from "express";
 
@@ -216,11 +108,9 @@ import dotenv from "dotenv";
 dotenv.config();
 const router = Router();
 
-const openAIApiKey = "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";
-const supabaseUrl = "https://awmkztjrwjqacpnxywoy.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF3bWt6dGpyd2pxYWNwbnh5d295Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI1ODU3NDQsImV4cCI6MjAyODE2MTc0NH0.6q_7mZ2BKAr7BfOvim9Y7Kop_RgddkN8BhMS3pLqmyg";
-
+const openAIApiKey = process.env.openAIApiKey;
+const supabaseUrl = process.env.supabaseUrl;
+const supabaseKey = process.env.supabaseKey;
 const datasource = new DataSource({
   type: "mysql",
   database: "vhack",
@@ -387,7 +277,7 @@ const chain = RunnableSequence.from([
     original_input: new RunnablePassthrough(),
   },
   {
-    context: retrieverChain,
+    // context: retrieverChain,
     // question: ({ original_input }) => original_input.question,
     question: (prev) => prev.standalone_question,
 
